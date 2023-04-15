ade104b98f502474ae16931da3e64889bfb9153d
Leonardo Anders <leonardoanders15@gmail.com>
2023-04-15 16:20:40 -0300

    arrowFunction

---
+++ Library/Function/arrowFunction.js
@@ -0,0 +1,10 @@
+let dobro = function (a) {
+   return a * 2
+}
+
+dobro = (a) => {
+   return 2 * a 
+}
+
+dobro = a => 2 * a //return implícito
+console.log(dobro(Math.PI)) 
---
+++ Library/Function/arrowFunction02.js
@@ -0,0 +1,11 @@
+ function pessoa() {
+   this.idade = 0
+
+   setInterval(() =>{
+     this.idade++
+     console.log(this.idade);
+ },2000)
+
+ }
+
+ new pessoa 
