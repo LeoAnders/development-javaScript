608f7ed2f662d36c66b72045c4d7d19a5abc5c49
LeoAnders <leonardoanders15@gmail.com>
2023-06-14 18:55:44 -0300

    atualizando diretorio das pastas

--- Tools/Node.js/Template_engine-EJS/index.js
+++ Tools/Node.js/Template_engine-EJS/index.js
@@ -1,35 +1,18 @@
 
 const express = require("express");
 const fs = require("fs");
+const path = require("path")
 const app = express();
 
-let user = {
-  id: 0,
-  name:"Leonardo",
-  phone:"(99)99999-9999"
-}
 
-//funcao de loop do meu objeto que busca la no meu template essas chaves se essas chaves existirem la eu vou substituir pelo valor do meu objeto
-function render(data, obj){
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
 
-for (let key in obj) {
-  data = data.replace(`{{{${key}}}}`, obj[key]);
-}
 
-return data 
+app.get("/", (req, res)=> {
 
-}
+  res.render("user")
 
-app.get("/", (req, res)=>{
-
-  fs.readFile("./templates/user.html", "UTF8", (err, data )=>{
-    if (!err) {
-
-      res.send(render(data, user))
-
-    }
-    
-  })
 
 })
 
